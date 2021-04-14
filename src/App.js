0105fa1722a6f5618ce94fd27b711e7fd6bbc589
Irina Kramer <irakramer@gmail.com>
2021-04-13 22:32:18 -0700

    App general layout

--- src/App.js
+++ src/App.js
@@ -3,10 +3,20 @@ import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <div>
-      <NavBar />
-      
-    </div>
+    <>
+    <header>
+      <NavBar />     
+    </header>
+
+    <main>
+
+    </main>
+
+    <footer>
+      <p>Final project for General Assembly React Development course</p>
+      <p>Copyright &copy; Irina Kramer 2021</p>
+    </footer>
+    </>
   );
 }
 
