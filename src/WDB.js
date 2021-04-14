3ed0274cb6f73ee919f510929a4546be3d417e72
Irina Kramer <irakramer@gmail.com>
2021-04-14 00:29:43 -0700

    Added data file

---
+++ src/WDB.js
@@ -0,0 +1,166 @@
+// Wine Varietals DB
+const WDB = {
+    wines: [
+        {
+            "id": 1,
+            "name": "Chardonnay",
+            "variety": "white",
+            "aromas": null,
+            "barrel-aged": null,
+            "fruits": {
+                "lemon": "images/lemon.png",
+                "lime": "images/lime.png",
+                "apple": "images/apple.png",
+                "fresh almond": "images/fresh_almond.png",
+                "pear": "images/pear.png"
+            },
+            "flowers": {
+                "lime blossom": "images/lime_blossom.png",
+                "acacia": "images/acacia.png",
+                "honeysuckle": "images/honeysuckle.png",
+                "verbena": "images/verbena.png",
+                "honey": "images/honey.png"
+            },
+            "herbs & spices": null,
+            "bonus": {
+                "butter": "images/butter.png",
+                "hazelnut": "images/hazelnut.png",
+                "toasted almond": "images/toasted_almond.png",
+                "vanilla": "images/vanilla.png",
+                "toast": "images/toast.png"
+            },
+            "sweet version": null,
+            "with bottle age": null,
+            "overview": "It is versatile and changes its character according to region, terroir and winemaker. It can be very fruity or very floral, and it can also reveal sharp, mineral flavors in the north of Burgundy, as Chablis does, or it can be sensual or buttery like thosr from California. ",
+            "where does it come from": [
+                "France: Burgundy, Champaigne, Jura, Languedoc, Provence",
+                "United States: mostly California",
+                "Canada",
+                "Chile",
+                "Argentina",
+                "South Africa",
+                "China",
+                "Australia"
+            ]
+        },
+        {
+            "id": 2,
+            "name": "Sauvignon Blank",
+            "variety": "white",
+            "aromas": null,
+            "barrel-aged": null,
+            "fruits": {
+                "lemon": "images/lemon.png",
+                "lime": "images/lime.png",
+                "grapefruit": "images/grapefruit.png",
+                "bergamot": "images/bergamot.png",
+                "pineapple": "images/pineapple.png",
+                "passion fruit": "images/passion_fruit.png"
+            },
+            "flowers": {
+                "jasmine": "images/jasmine.png",
+                "elderberry": "images/elderberry.png"
+            },
+            "herbs & spices": null,
+            "bonus": {
+                "smoke": "images/smoke.png",
+                "chalk": "images/chalk.png"
+            },
+            "sweet version": null,
+            "with bottle age": null,
+            "overview": "It's one of the most expressive varietals. It has refreshing aromas and notes of bright citrus, and can be reminiscent of tender grass that evokes springtime and the joy of living. ",
+            "where does it come from": [
+                "France: central Loire Valley, Bordeaux, South West",
+                "Spain",
+                "New Zealand",
+                "United States (California)",
+                "Chile",
+                "South Africa"
+            ]
+        },
+        {
+            "id": 3,
+            "name": "Pinot Noir",
+            "variety": "red",
+            "aromas": {
+                "cherry": "images/cherry.png",
+                "raspberry": "images/raspberry.png",
+                "strawberry": "images/strawberry.png",
+                "black currant": "images/black_currant.png",
+                "iris": "images/iris.png",
+                "violet": "images/violet.png"
+            },
+            "barrel-aged": {
+                "wood": "images/wood.png",
+                "vanilla": "images/vanilla.png",
+                "cinnamon": "images/cinnamon.png",
+                "tobacco": "images/tobacco.png"
+            },
+            "fruits": null,
+            "flowers": null,
+            "herbs & spices": null,
+            "bonus": null,
+            "sweet version": null,
+            "with bottle age": {
+                "fur": "images/fur.png",
+                "leather": "images/leather.png",
+                "undergrowth": "images/undergrowth.png",
+                "moss": "images/moss.png",
+                "truffle": "images/truffle.png",
+                "musk": "images/musk.png"
+            },
+            "overview": "The king of Burgundies shows as much finesse as power. The wine is a brilliant ruby color, with bewitching aromas of red fruit, and in the mouth its texture is fine and silky with low tannins.",
+            "where does it come from": [
+                "Europe",
+                "North America",
+                "South America",
+                "South Africa",
+                "France: Burgundy, Chamaigne",
+                "United States: California, Oregon",
+                "New Zealand",
+                "Australia"
+            ]
+        },
+        {
+            "id": 4,
+            "name": "Cabernet Sauvignon",
+            "variety": "red",
+            "aromas": {
+                "black currant": "images/black_currant.png",
+                "blackberry": "images/blackberry.png",
+                "fern": "images/fern.png",
+                "bell pepper": "images/bell_pepper.png",
+                "jasmine": "images/jasmine.png",
+                "sandalwood": "images/sandalwood.png",
+                "pine resin": "images/pine_resin.png"
+            },
+            "barrel-aged": {
+                "oak": "images/oak.png",
+                "vanilla": "images/vanilla.png",
+                "clove": "images/clove.png",
+                "licorice": "images/licorice.png"
+            },
+            "fruits": null,
+            "flowers": null,
+            "herbs & spices": null,
+            "bonus": null,
+            "sweet version": null,
+            "with bottle age": {
+                "leather": "images/leather.png",
+                "tobacco": "images/tobacco.png",
+                "wild game": "images/wild_game.png",
+                "cedar": "images/cedar.png",
+                "pencil shavings": "images/pencil_shavings.png",
+                "truffle": "images/truffle.png"
+            },
+            "overview": "This is another monarch variety, this time from Bordeaux. This grape is the marathon runner in the wine world. Wine made from these grapes is made to last. Its abundant tannins are an invitation to wait decades for the wine to develop a powerful bouquet and extremely complex aromas of cassis, tobacco, wild game, and cedar.",
+            "where does it come from": [
+                "All over, from France to China",
+                "France: Bordeaux, South of France",
+                "Italy",
+                "Chile",
+                "United States: California"
+            ]
+        }
+    ]
+}
