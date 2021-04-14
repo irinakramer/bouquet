95bd8ee475a7f0e292f8efc0bb6ca2e1f51df296
Irina Kramer <irakramer@gmail.com>
2021-04-14 11:23:14 -0700

    Filtered list for Whites and Reds

--- src/Wines.js
+++ src/Wines.js
@@ -2,11 +2,26 @@ import WineRow from './WineRow';
 
 const Wines = ({wines}) => {
 
-    const allWines = wines.map( w => <WineRow key={w.id} name={w.name} variety={w.variety}/>)
+    //const allWines = wines.map( w => <WineRow key={w.id} name={w.name} variety={w.variety}/>)
+
+    const whiteWines = wines.filter( (w => w.variety === "white"))
+    const allWhiteWines = whiteWines.map(ww => 
+        <WineRow key={ww.id} name={ww.name} variety={ww.variety}/>
+    )
+
+    const redWines = wines.filter( (w => w.variety === "red"))
+    const allRedWines = redWines.map(rw => 
+        <WineRow key={rw.id} name={rw.name} variety={rw.variety}/>
+    )
+
     return (
         <div>
             <h1>All Wines</h1>
-            {allWines}
+            <h2>Whites:</h2>
+            {allWhiteWines}
+
+            <h2>Reds:</h2>
+            {allRedWines}
         </div>
     )
 }
