1f53073ad0f3c4d173896fe16152915fb7517d94
Irina Kramer <irakramer@gmail.com>
2021-04-14 17:43:12 -0700

    Instal Router, set up basic routing

--- src/App.js
+++ src/App.js
@@ -1,27 +1,41 @@
+import { Switch, Route, Link, Redirect } from 'react-router-dom';
 import './App.css';
 import NavBar from './components/NavBar';
 import Search from './Search';
 import Wines from './Wines';
+import Wine from './Wine';
 import WDB from './WDB';
 
 const App = () => {
   console.log(WDB.wines)
   return (
     <>
-    <header>
-      <NavBar />     
-    </header>
+      <header>
+        <NavBar />     
+      </header>
 
-    <main>
-      <Search />
-      <Wines wines={WDB.wines} />
+      <main>
+        <Search />
+        
+        {/* TODO: link to All Wines */}
+        <Link to="/wines">All Wines</Link>
 
-    </main>
+        <Wines wines={WDB.wines} />
 
-    <footer>
-      {/* <p>Final project for General Assembly React Development course</p>
-      <p>Copyright &copy; Irina Kramer 2021</p> */}
-    </footer>
+        {/* TODO: Set up proper routing */}
+        <Switch>
+          <Route path="/search" component={Search} />
+          <Route path="/wines" component={Wines} />
+          <Route path="/wines/:name" render={routerProps =>
+            <Wine {...routerProps} />
+          } />
+        </Switch>
+      </main>
+
+      <footer>
+        {/* <p>Final project for General Assembly React Development course</p>
+        <p>Copyright &copy; Irina Kramer 2021</p> */}
+      </footer>
     </>
   );
 }
