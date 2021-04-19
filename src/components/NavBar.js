6251140a361e4019598190439747189eab7ab7b4
Irina Kramer <irakramer@gmail.com>
2021-04-19 10:15:06 -0700

    MUI navbar, installed icons

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,22 @@
 import {Link} from 'react-router-dom';
 import AppBar from '@material-ui/core/AppBar';
 import ToolBar from '@material-ui/core/Toolbar';
-import Typography from "@material-ui/core/Typography"
+import Typography from "@material-ui/core/Typography";
+import Button from '@material-ui/core/Button';
+import HomeIcon from '@material-ui/icons/Home';
 
 const NavBar = () => {
     return(
         <div>
             <AppBar position="static">
                 <ToolBar>
-                    <Typography variant="body2" color="inherit">
-                        <Link to="/">Bouquet Wine Tasting</Link>
-                    </Typography>
+                    <Button 
+                        component={Link} 
+                        startIcon={<HomeIcon />}
+                        to={"/"} 
+                        color="inherit">
+                            Bouquet Wine Tasting
+                    </Button>           
                 </ToolBar>
             </AppBar>
         </div>
