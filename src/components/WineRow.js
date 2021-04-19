3f73b3f6b6c40f67827eba0c654405a7ba5aa39b
Irina Kramer <irakramer@gmail.com>
2021-04-19 14:01:26 -0700

    MUI wine row

--- src/components/WineRow.js
+++ src/components/WineRow.js
@@ -1,9 +1,29 @@
 import {Link} from 'react-router-dom';
+import {makeStyles} from '@material-ui/core';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+
+const useStyles = makeStyles({
+    white: {
+        backgroundColor: "beige"
+    },
+    red: {
+        backgroundColor: "lightpink"
+    }
+})
 
 const WineRow = ({objectId, name, variety}) => {
+    const classes = useStyles();   
     return (
         <div>
-            <Link to={`/wines/${objectId}`}>{name} - {variety}</Link>
+            <List>
+                <ListItem button 
+                    component={Link}
+                    className={`${variety === "white" ? classes.white : classes.red}`} 
+                    to={`/wines/${objectId}`}>
+                        {name} - {variety}
+                </ListItem>
+            </List>
             
         </div>
     )
