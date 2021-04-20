05195217b19a466c881effbcbf9d373a8456ef6c
Irina Kramer <irakramer@gmail.com>
2021-04-20 15:34:44 -0700

    MUI layout Wine.js

--- src/pages/Wine.js
+++ src/pages/Wine.js
@@ -3,9 +3,31 @@ import {Link, Redirect} from 'react-router-dom';
 import Chart from "react-google-charts";
 import WinesAPI from '../services/WinesAPI';
 import Flavor from '../components/Flavor';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import Button from '@material-ui/core/Button';
+import {makeStyles} from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import GridList from '@material-ui/core/GridList';
+import Avatar from '@material-ui/core/Avatar';
 
+const useStyles = makeStyles( (theme) => ({
+    tbd: {
+        backgroundColor: theme.palette.background.paper,
+        padding: theme.spacing(8, 0, 6)
+    },
+    avatar: {
+        width: theme.spacing(10),
+        height: theme.spacing(10),
+        fontSize: '1rem',
+        textAlign: 'center',
+        wordSpacing: '9999px',
+    },
+}))
 
 const Wine = ({match}) => {
+
+    const classes = useStyles();
 
     const [wine, setWine] = useState({});
     const [redirect, setRedirect] = useState({});
@@ -34,7 +56,6 @@ const Wine = ({match}) => {
             setRedirect( {pathname: "/"} )
         else
             setRedirect( {pathname: "/404"})
-
     }
 
     if (redirect.pathname) {
@@ -55,31 +76,155 @@ const Wine = ({match}) => {
             sweetVersion,
             regions} = wine;
 
+            console.log(fruits, aromas, flowers, bonus)
+
+
     return (
         <div>
-            <h1>{name}</h1>     
-            <ul>Fruits: {fruits && fruits.map( (item, i) => <Flavor key={i} value={item} />)}</ul>
-            <ul>Flowers: {flowers && flowers.map( (item, i) => <Flavor key={i} value={item} />)}</ul>
-            <ul>Bonus: {bonus && bonus.map( (item, i) => <Flavor key={i} value={item} />)}</ul>
-            <ul>{aromas && aromas.map( (item, i) => <Flavor key={i} value={item} />)}</ul>
+            <Container className={""} maxWidth="md">
+                <Typography component="h1" variant="h2" align="center" color="textPrimary">
+                    {name}
+                </Typography>
 
-            <h3>Overview:</h3>
-            <p>{overview}</p>
+            {/* All Flavors */} 
+            <Grid container spacing={4}>
+                {
+                    !fruits || !fruits.length ? null :
+                    <>               
+                        <Grid item xs={3}>
+                            <Avatar className={classes.avatar}>Fruits</Avatar>
+                        </Grid>
+                        <Grid item xs={9}>
+                            <GridList cols={5}>
+                                {fruits.map( (item, i) => <Flavor key={i} flavor={item} />)}
+                            </GridList>
+                        </Grid>
+                    </>
+                }
 
-            <h3>Where does it come from?</h3>
+                {
+                    !flowers || !flowers.length? null :
+                    <>               
+                        <Grid item xs={3}>
+                            <Avatar className={classes.avatar}>Flowers</Avatar>
+                        </Grid>
+                        <Grid item xs={9}>
+                            <GridList cols={5}>
+                                {flowers.map( (item, i) => <Flavor key={i} flavor={item} />)}
+                            </GridList>
+                        </Grid>
+                    </>
+                }
 
-                <Chart
-                    width={'500px'}
-                    height={'300px'}
-                    chartType="GeoChart"
-                    data={regions && ["Country", ...regions].map(r => Array(r))}
-                    mapsApiKey={process.env.REACT_APP_GEOCHARTS_API_KEY}
-                    rootProps={{ 'data-testid': '1' }}
-                />
+                {
+                    !aromas || !aromas.length ? null :
+                    <>               
+                        <Grid item xs={3}>
+                            <Avatar className={classes.avatar}>Aromas</Avatar>
+                        </Grid>
+                        <Grid item xs={9}>
+                            <GridList cols={5}>
+                                {aromas.map( (item, i) => <Flavor key={i} flavor={item} />)}
+                            </GridList>
+                        </Grid>
+                    </>
+                }
 
-           <Link to={`/wines/${objectId}/edit`}>Edit</Link>
-           <br></br><br></br><br></br>
-           <Link to={""} onClick={handleDelete}>Delete</Link>
+                {
+                    !withBottleAge || !withBottleAge.length ? null :
+                    <>               
+                        <Grid item xs={3}>
+                            <Avatar className={classes.avatar}>with Bottle Age</Avatar>
+                        </Grid>
+                        <Grid item xs={9}>
+                            <GridList cols={5}>
+                                {withBottleAge.map( (item, i) => <Flavor key={i} flavor={item} />)}
+                            </GridList>
+                        </Grid>
+                    </>
+                }
+
+                {
+                    !barrelAged || !barrelAged.length ? null :
+                    <>               
+                        <Grid item xs={3}>
+                            <Avatar className={classes.avatar}>Barrel Aged</Avatar>
+                        </Grid>
+                        <Grid item xs={9}>
+                            <GridList cols={5}>
+                                {barrelAged.map( (item, i) => <Flavor key={i} flavor={item} />)}
+                            </GridList>
+                        </Grid>
+                    </>
+                }
+
+                {
+                    !bonus || !bonus.length ? null :
+                    <>               
+                        <Grid item xs={3}>
+                            <Avatar className={classes.avatar}>bonus</Avatar>
+                        </Grid>
+                        <Grid item xs={9}>
+                            <GridList cols={5}>
+                                {bonus.map( (item, i) => <Flavor key={i} flavor={item} />)}
+                            </GridList>
+                        </Grid>
+                    </>
+                }
+            </Grid>
+            {/* End All Flavors */} 
+
+            
+            <Grid container spacing={4}>
+                <Grid item xs={3}>
+                    <Typography component="h2" variant="h6" color="textPrimary">
+                        Overview:
+                    </Typography>
+                    <Typography variant="body1" color="textPrimary" paragraph>
+                        {overview}
+                    </Typography>
+                </Grid>
+                <Grid item xs={9}>
+                    <Typography component="h2" variant="h6" color="textPrimary">
+                        Where does it come from?
+                    </Typography>
+                    <Chart
+                        width={'500px'}
+                        height={'300px'}
+                        chartType="GeoChart"
+                        data={regions && ["Country", ...regions].map(r => Array(r))}
+                        mapsApiKey={process.env.REACT_APP_GEOCHARTS_API_KEY}
+                        rootProps={{ 'data-testid': '1' }}
+                    />
+                </Grid>
+            </Grid>
+
+
+            {/* Buttons */} 
+            <Container maxWidth="sm" align="center">
+                <Button 
+                    component={Link} 
+                    to={`/wines/${objectId}/edit`} 
+                    variant="outlined" 
+                    color="primary"
+                    size="small">
+                    Edit Wine
+                </Button> 
+                <Button 
+                    component={Link} 
+                    to={""} 
+                    onClick={handleDelete}
+                    variant="outlined" 
+                    color="primary"
+                    size="small">
+                    Delete Wine
+                </Button> 
+
+            </Container>
+            {/* End Buttons */} 
+
+
+           </Container>
         </div>
     )
 }
