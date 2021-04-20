bbd371381157329c9f386c21f9397ef9af82a3d8
Irina Kramer <irakramer@gmail.com>
2021-04-20 13:24:38 -0700

    MUI flavor component

--- src/components/Flavor.js
+++ src/components/Flavor.js
@@ -1,7 +1,25 @@
+import Typography from '@material-ui/core/Typography';
+import {makeStyles} from '@material-ui/core';
+
+const useStyles = makeStyles( theme => ({
+    listItem: {
+        padding: theme.spacing(2)
+    }
+}))
+
 const Flavor = (props) => {
+
+    const classes = useStyles();
+
+    const imageName = props.flavor.split(' ').join('_');
     
     return (
-        <li><img src={process.env.PUBLIC_URL + `/images/${props.value}.png`} />{props.value}</li>
+        <li className={classes.listItem}>
+            <img src={process.env.PUBLIC_URL + `/images/${imageName}.png`} />
+            <Typography variant="caption" align="center" color="textPrimary" display="block" >
+                {props.flavor}
+            </Typography>         
+        </li>
     )
 }
 
